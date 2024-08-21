cab63725423d9107b1a7310894267e815bfb7ee6
WillyRumapea <willyrumapea123jr@gmail.com>
2024-08-21 10:02:38 +0700

    mobile layout tes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,16 @@
 const bookingButton = document.getElementById("booking-button");
+const contactButton = document.getElementById("contact-button");
+const hamMenu = document.getElementsByClassName("ham-menu")[0];
+const navBar = document.getElementById("navbar-nav");
 
 bookingButton.addEventListener("click", () => {
   window.location.href = "service/booking.html";
+});
+
+contactButton.addEventListener("click", () => {
+  window.scroll(0, 1300);
+});
+
+hamMenu.addEventListener("click", () => {
+  navBar.style.right = "100px";
 });
