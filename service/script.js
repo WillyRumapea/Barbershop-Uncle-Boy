2ce47229dabab7b6db33ef9ff02b38fda5dea055
WillyRumapea <willyrumapea123jr@gmail.com>
2024-08-22 14:49:50 +0700

    button wa

--- service/script.js
+++ service/script.js
@@ -1,64 +1,54 @@
-const listHargaCukur = {
-  Undercut: 25000,
-  "Comma Hair": 27000,
-  "Two Block": 25000,
-  "French Crop": 23000,
-  Mullet: 30000,
+const listStatus = {
+  tk: 10000,
+  remaja: 15000,
+  praDewasa: 20000,
+  dewasa: 25000,
 };
 
-const listStatus = {
-  anak: 3000,
-  remaja: 5000,
-  dewasa: 7000,
+const listServis = {
+  Pangkas: 0,
+  "Warnai Rambut": 20000,
 };
 
 const umurCustomer = document.getElementById("umur-customer");
+const bookingForm = document.getElementById("booking-form");
+const services = document.getElementById("servis");
+const harga = document.getElementById("harga-cukur");
+let statusCustomer = 0;
 
 function updateStatusCustomer() {
-  if (umurCustomer.value <= 15) {
-    statusCustomer = listStatus.anak;
-  } else if (umurCustomer.value > 15 && umurCustomer.value < 20) {
+  if (umurCustomer.value >= 19) {
+    statusCustomer = listStatus.dewasa;
+  } else if (umurCustomer.value >= 16) {
+    statusCustomer = listStatus.praDewasa;
+  } else if (umurCustomer.value >= 7) {
     statusCustomer = listStatus.remaja;
   } else {
-    statusCustomer = listStatus.dewasa;
+    statusCustomer = listStatus.tk;
   }
 }
 
-let statusCustomer = 0;
+// services.addEventListener("input", (service) => {
+//   const serviceSelected = service.target.value;
 
-document.getElementById("tipe-cukur").addEventListener("input", (e) => {
-  const styleSelected = e.target.value;
-  let hargaCukur = document.getElementById("harga-cukur");
+//   if (serviceSelected === "Warnai Rambut") {
+//     harga.value = listServis["Warnai Rambut"];
+//   } else {
+//     updateStatusCustomer();
+//     harga.value = updateStatusCustomer();
+//   }
+// });
 
-  if (!umurCustomer) {
-    hargaCukur.value = "";
+bookingForm.addEventListener("input", () => {
+  if (umurCustomer.value === "" && services.value === "") {
+    harga.value = "";
     return;
   }
 
-  updateStatusCustomer(umurCustomer);
-
-  hargaCukur.innerHTML = "";
-
-  if (listHargaCukur[styleSelected]) {
-    hargaCukur.value = listHargaCukur[styleSelected] + statusCustomer;
+  if (services.value === "Warnai Rambut") {
+    harga.value = listServis["Warnai Rambut"];
   } else {
-    hargaCukur.value = "";
-  }
-});
-
-document.getElementById("umur-customer").addEventListener("input", (e) => {
-  const umurCustomer = e.target.value;
-  let hargaCukur = document.getElementById("harga-cukur");
-
-  if (!umurCustomer) {
-    hargaCukur.value = "";
-    return;
-  }
-
-  updateStatusCustomer(umurCustomer);
-
-  const styleSelected = document.getElementById("tipe-cukur").value;
-  if (listHargaCukur[styleSelected]) {
-    hargaCukur.value = listHargaCukur[styleSelected] + statusCustomer;
+    updateStatusCustomer();
+    harga.value = statusCustomer;
   }
 });
